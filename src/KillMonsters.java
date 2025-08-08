4040a69ffc875c0c48c6faba058ea315c5701b89
Alcom01 <alkimsabancilar6@gmail.com>
2025-08-08 18:22:09 +0300

    class renamed  to a  appropraite name

--- src/KillMonsters.java
+++ src/KillMonsters.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class KillMonsters {
     public static void main(String[] args) {
        int[] monsters1 = {3,0,3,4,1};
        int[] monsters2 =  {5,4,3,2,1};
